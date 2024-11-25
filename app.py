c0879688e0727e37257e00788da04d43f7fe9070
zonehisa <nijipapazoneokun123@gmail.com>
2024-11-25 18:03:07 +0900

    config databaseあたり作った

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+from config import User
+
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
- 
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000, debug=True)
---
+++ config.py
@@ -0,0 +1,17 @@
+from peewee import SqliteDatabase, Model, IntegerField, CharField, TextField
+
+db = SqliteDatabase("db.sqlite")
+
+
+class User(Model):
+    id = IntegerField(primary_key=True)
+    name = CharField(unique=True)
+    email = CharField(unique=True)
+    password = TextField()
+
+    class Meta:
+        database = db
+        table_name = "users"
+
+
+db.create_tables([User])
